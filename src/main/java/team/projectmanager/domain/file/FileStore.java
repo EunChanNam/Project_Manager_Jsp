c8de86851a305e0749114bc0832f5127ba8f000f
eunchannam <namhm23@gmail.com>
2022-11-28 11:09:08 +0900

    dev : FileStore class 작성

---
+++ src/main/java/team/projectmanager/domain/file/FileStore.java
@@ -0,0 +1,47 @@
+package team.projectmanager.domain.file;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public UploadFile storeFile(MultipartFile multipartFile) {
+        if (!multipartFile.isEmpty()) {
+            String originalFilename = multipartFile.getOriginalFilename();
+            String storeFilename = createStoreFilename(originalFilename);
+            String fullPath = getFullPath(storeFilename);
+
+            //IOException 잡아서 사용자 정의 Exception 으로 변경
+            try {
+                multipartFile.transferTo(new File(fullPath));
+            } catch (IOException e) {
+                return null;
+            }
+
+            return new UploadFile(originalFilename, storeFilename);
+        } else return null;
+    }
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    private String createStoreFilename(String originalFilename) {
+        String ext = getExt(originalFilename);
+        return UUID.randomUUID().toString() + ext;
+    }
+
+    private String getExt(String originalFilename) {
+        int index = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(index);
+    }
+}
